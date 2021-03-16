e8eb664d6acf0a4b041e696335aa3fedd34650a0
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-16 16:13:37 -0400

    basic recipe list functionality

--- ng-practice-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng-practice-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/models/ingredient.model';
+import { Recipe } from '../../models/recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'Enchiladas',
+      `Delicious Enchiladas...`,
+      'https://www.yellowblissroad.com/wp-content/uploads/2020/02/Ground-Beef-Enchiladas-social.jpg',
+      [new Ingredient('Tortillas', 2), new Ingredient('Beef (1LB)', 1)])
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectRecipe = (recipe: Recipe) => {
+    this.recipeSelected.emit(recipe);
   }
 
 }
