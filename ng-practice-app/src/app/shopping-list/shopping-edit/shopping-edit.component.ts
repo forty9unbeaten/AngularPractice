3d32881d1e58cbe5b1f44f420e20347cfacd2735
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-16 16:14:32 -0400

    basic shopping edit functionality

--- ng-practice-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ng-practice-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() addIngredient = new EventEmitter<Ingredient>();
+  @Output() deleteIngredient = new EventEmitter<Ingredient>();
+  @Input() selectedIngredient: Ingredient;
+  @ViewChild('deleteButton') deleteButton: ElementRef;
+  @ViewChild('addButton') addButton: ElementRef;
+
+  ingName = '';
+  ingQuantity = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validClear = (): boolean => {
+    return this.ingName !== '' || this.ingQuantity !== 0 ? true : false;
+  }
+
+  validAdd = (): boolean => {
+    return this.ingName !== '' && this.ingQuantity !== 0 ? true : false ;
+  }
+
+  clearInputs = (): void => {
+    this.ingName = '';
+    this.ingQuantity = 0;
+  }
+
+  onAddIngredient = (): void => {
+    this.addIngredient.emit(
+      new Ingredient(this.ingName, this.ingQuantity)
+    );
+    this.clearInputs();
+    this.addButton.nativeElement.disabled = true;
+  }
+
+  onDeleteIngredient = (ingredient: Ingredient): void => {
+    this.deleteIngredient.emit(ingredient);
+    this.deleteButton.nativeElement.disabled = true;
+  }
 }
