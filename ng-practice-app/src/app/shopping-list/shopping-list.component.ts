3e294d949ed04885df1a001e2388459032869973
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-16 16:14:19 -0400

    basic shopping list component functionality

--- ng-practice-app/src/app/shopping-list/shopping-list.component.ts
+++ ng-practice-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('tomato', 10),
+    new Ingredient('lettuce', 14)
+  ];
+  selectedIngredient: Ingredient;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onIngredientAdded = (ingredient: Ingredient): void => {
+    this.ingredients.push(ingredient);
+  }
+
+  selectIngredient = (ingredient: Ingredient): void => {
+    this.selectedIngredient = ingredient;
+  }
+
+  onIngredientDeleted = (ingredient: Ingredient): void => {
+    this.ingredients.splice(
+      this.ingredients.indexOf(ingredient),
+      1
+    );
   }
 
 }
