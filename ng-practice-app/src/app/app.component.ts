c5c3280ed416255753773eb1c5e42cb778b1642a
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-22 11:04:14 -0400

    add to list switches tabs automatically

--- ng-practice-app/src/app/app.component.ts
+++ ng-practice-app/src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
+import { RecipesService } from './services/recipes.service';
 import { ShoppingListService } from './services/shopping-list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ShoppingListService]
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'ng-practice-app';
-  activeTab = 'r';
+  public title = 'ng-practice-app';
+  public activeTab = 'r';
+
+  constructor(private recipesService: RecipesService) {
+    this.recipesService.switchToShoppingList.subscribe(() => {
+      this.activeTab = 's';
+    });
+  }
 
   switchTabs = (tab: string): void => {
     this.activeTab = tab;
