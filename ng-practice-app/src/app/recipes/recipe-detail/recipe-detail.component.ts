7c8c4f85c1bf6387676cb7d5f72a2da859759e61
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-23 12:15:17 -0400

    recipe selection and redirection behavior handled by routing

--- ng-practice-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ng-practice-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
 import { Recipe } from 'src/app/models/recipe.model';
 import { RecipesService } from 'src/app/services/recipes.service';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
@@ -11,19 +12,26 @@ import { ShoppingListService } from 'src/app/services/shopping-list.service';
 export class RecipeDetailComponent implements OnInit {
   public recipe: Recipe;
 
-  constructor(private recipesService: RecipesService, private shoppingList: ShoppingListService) {}
+  constructor(
+    private recipesService: RecipesService,
+    private shoppingList: ShoppingListService,
+    private router: Router,
+    private currentRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.recipe = this.recipesService.getSelectedRecipe();
+    this.recipe = this.recipesService.getSelectedRecipe(
+      Number(this.currentRoute.snapshot.params.id)
+    );
   }
 
   public onDeleteRecipe = (): void => {
     this.recipesService.deleteRecipe(this.recipe);
+    this.router.navigate(['/recipes']);
   }
 
   public onAddToShoppingList = (): void => {
     this.shoppingList.addIngredients(this.recipe.ingredients);
-    this.shoppingList.navToShoppingList.emit();
+    this.router.navigate(['/shopping-list']);
   }
 
 }
