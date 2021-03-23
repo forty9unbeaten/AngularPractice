c1b88904b87c2329219d34078dc6d43337e28039
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-23 12:09:50 -0400

    get selected recipe by id through routing

--- ng-practice-app/src/app/services/recipes.service.ts
+++ ng-practice-app/src/app/services/recipes.service.ts
@@ -8,41 +8,49 @@ import { Ingredient } from '../models/ingredient.model';
 export class RecipesService {
   private recipes: Recipe[] = [
     new Recipe(
+      1,
       'Enchiladas',
       `Delicious Enchiladas...`,
       'https://www.yellowblissroad.com/wp-content/uploads/2020/02/Ground-Beef-Enchiladas-social.jpg',
       [new Ingredient('Tortillas', 2), new Ingredient('Beef (1LB)', 1)]
     )
   ];
-  private selectedRecipe: Recipe;
-  public newRecipeSelected = new EventEmitter();
+  private generatedId = this.recipes.slice(-1)[0].id + 1;
   public recipesChanged = new EventEmitter<Recipe[]>();
-  public recipeDeleted = new EventEmitter();
   public switchToShoppingList = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+  }
 
-  public getRecipes = (): Recipe[] => {
+  public getRecipeList = (): Recipe[] => {
     return this.recipes.slice();
   }
 
-  public getSelectedRecipe = (): Recipe => {
-    return this.selectedRecipe;
+  public setRecipeList = (recipes: Recipe[]): void => {
+    this.recipes = recipes;
+    this.recipesChanged.emit(this.recipes.slice());
   }
 
-  public setSelectedRecipe = (recipe: Recipe): void => {
-    this.selectedRecipe = recipe;
-    this.newRecipeSelected.emit();
+  public getSelectedRecipe = (id: number): Recipe => {
+    return this.recipes.find(recipe => recipe.id === id);
   }
 
-  public addRecipe = (recipe: Recipe): void => {
-    this.recipes.push(recipe);
+  public addRecipe = (name: string, description: string, imgPath: string, ingredients: Ingredient[]): void => {
+    this.recipes.push(
+      new Recipe(
+        this.generatedId,
+        name,
+        description,
+        imgPath,
+        ingredients
+      )
+    );
+    this.generatedId++;
     this.recipesChanged.emit(this.recipes.slice());
   }
 
   public deleteRecipe = (recipe: Recipe): void => {
     this.recipes = this.recipes.filter(existingRecipe => existingRecipe !== recipe);
     this.recipesChanged.emit(this.recipes.slice());
-    this.recipeDeleted.emit();
   }
 }
