787b131a751adc541a8afd21601b0a4132844ee5
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-23 13:36:23 -0400

    implment 404 error route redirection

--- ng-practice-app/src/app/routing/routing.module.ts
+++ ng-practice-app/src/app/routing/routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from '../recipes/recipes.component';
 import { RecipeDetailComponent } from '../recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
+import { Error404Component } from '../error404/error404.component';
 
 const appRoutes: Routes = [
   { path: 'recipes', component: RecipesComponent, children: [
@@ -10,7 +11,8 @@ const appRoutes: Routes = [
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-
+  { path: 'not-found', component: Error404Component },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
