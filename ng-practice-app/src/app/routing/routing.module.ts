bf283f1267b2983b76047694fbf84b0f5ca021cb
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-23 15:54:58 -0400

    add routes for home, edit and new recipe components

--- ng-practice-app/src/app/routing/routing.module.ts
+++ ng-practice-app/src/app/routing/routing.module.ts
@@ -4,10 +4,15 @@ import { RecipesComponent } from '../recipes/recipes.component';
 import { RecipeDetailComponent } from '../recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from '../shopping-list/shopping-list.component';
 import { Error404Component } from '../error404/error404.component';
+import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
+import { RecipeHomeComponent } from '../recipes/recipe-home/recipe-home.component';
 
 const appRoutes: Routes = [
   { path: 'recipes', component: RecipesComponent, children: [
-    {path: ':id/:name', component: RecipeDetailComponent}
+    { path: 'new', component: RecipeEditComponent },
+    { path: ':id', component: RecipeDetailComponent },
+    { path: ':id/edit', component: RecipeEditComponent },
+    { path: '', component: RecipeHomeComponent, pathMatch: 'full' }
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
