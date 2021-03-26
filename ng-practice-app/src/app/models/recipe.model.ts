2808c28ee72a51407170651f42039e93b7885d5d
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-26 11:53:44 -0400

    add id property to recipe model

--- ng-practice-app/src/app/models/recipe.model.ts
+++ ng-practice-app/src/app/models/recipe.model.ts
@@ -1,11 +1,16 @@
 import { Ingredient } from './ingredient.model';
 
+
 export class Recipe {
     constructor(
-        public id: number,
         public name: string,
         public description: string,
         public imgPath: string,
-        public ingredients: Ingredient[]
-    ) {}
+        public ingredients: Ingredient[],
+        public id?: number
+    ) {
+        if (!this.id) {
+            this.id = 1;
+        }
+    }
 }
