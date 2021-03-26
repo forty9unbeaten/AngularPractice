60c24a3ee6fd69fd9e3ea97f49cd344e9643b50d
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-26 11:53:59 -0400

    remove unnecessary inports

--- ng-practice-app/src/app/recipes/recipes.component.ts
+++ ng-practice-app/src/app/recipes/recipes.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Recipe } from '../models/recipe.model';
-import { RecipesService } from '../services/recipes.service';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
-
-  constructor(private recipesService: RecipesService, private route: ActivatedRoute) {}
+  constructor() {}
 
   ngOnInit(): void {
   }
