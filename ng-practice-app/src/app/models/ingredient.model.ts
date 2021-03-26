6f57086a531f36c4c3db2ef9a6273b96ec5d37d3
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-26 11:53:10 -0400

    add measurement unit to ingredient model

--- ng-practice-app/src/app/models/ingredient.model.ts
+++ ng-practice-app/src/app/models/ingredient.model.ts
@@ -1,3 +1,7 @@
 export class Ingredient {
-    constructor(public name: string, public quantity: number){}
+    constructor(
+        public name: string, 
+        public quantity: number, 
+        public measurementUnit: string
+    ){}
 }
