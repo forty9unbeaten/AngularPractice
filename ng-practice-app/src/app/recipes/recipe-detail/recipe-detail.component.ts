128dc4d79dd8fc8602ad0ffdcd0047789616cc6c
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-30 14:24:52 -0400

    Load selected recipe from firebase API

--- ng-practice-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ng-practice-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -14,7 +14,9 @@ import { ShoppingListService } from 'src/app/services/shopping-list.service';
 })
 export class RecipeDetailComponent implements OnInit, OnDestroy {
 	public recipe: Recipe;
+	public dataLoading = false;
 	private routeSub: Subscription;
+	private recipeSub: Subscription;
 	
 	constructor(
 		private recipesService: RecipesService,
@@ -23,13 +25,34 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
 		private currentRoute: ActivatedRoute) {}
 
 	ngOnInit(): void {
+		this.dataLoading = true;
 		this.routeSub = this.currentRoute.params.subscribe((params: Params) => {
-			this.recipe = this.recipesService.getSelectedRecipe(Number(params.id));
+			this.recipesService.getSelectedRecipe(params.id)
+				.subscribe(
+					(recipe: Recipe) => {
+						if (recipe) {
+							this.recipe = recipe;
+							this.dataLoading = false;
+						} else {
+							this.router.navigate(['/not-found'])
+						}
+					},
+					error => {
+						console.log(error);
+					}
+				)
 		});
+		this.recipeSub = this.recipesService.recipeChanged.subscribe(
+			(recipe: Recipe) => {
+				this.recipe = recipe
+			}
+		)
+		
 	}
 
 	ngOnDestroy(): void {
 		this.routeSub.unsubscribe();
+		this.recipeSub.unsubscribe();
 	}
 
 	public onDeleteRecipe = (): void => {
