acb588b8a0e8facfdf329252ecb1d0f2d1f53d4c
forty9unbeaten <rob.sp3ars@gmail.com>
2021-03-30 14:26:26 -0400

    load recipes from firebase API

--- ng-practice-app/src/app/services/recipes.service.ts
+++ ng-practice-app/src/app/services/recipes.service.ts
@@ -1,56 +1,100 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
-import { Ingredient } from '../models/ingredient.model';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Enchiladas',
-      `Tasty enchiladas with red sauce. Just like your abuela used to make!`,
-      'https://www.yellowblissroad.com/wp-content/uploads/2020/02/Ground-Beef-Enchiladas-social.jpg',
-      [new Ingredient('Tortillas', 2, 'Packs'), new Ingredient('Beef', 1, 'Pound')]
-    )
-  ];
-
+  private recipes: Recipe[];
+  private recipesURL: string = 'https://angular-practice-8c0db-default-rtdb.firebaseio.com/recipes';
   public recipesChanged = new Subject<Recipe[]>();
-  private generatedId = this.recipes.slice(-1)[0].id + 1;
+  public recipeChanged = new Subject<Recipe>();
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
-  public getRecipeList = (): Recipe[] => {
-    return this.recipes.slice();
+  public getRecipeList = (): Observable<any> => {
+    return this.http.get(`${this.recipesURL}.json`).pipe(
+      map(
+        (res) => {
+          if (res) {
+            let recipesArray: Recipe[] = [];
+            for(let firebaseId in res) {
+              res[firebaseId].id = firebaseId;
+              if (!res[firebaseId].ingredients) {
+                res[firebaseId].ingredients = [];
+              }
+              recipesArray.push(res[firebaseId]);
+            }
+            this.recipes = recipesArray;
+            return recipesArray;
+          } else {
+            this.recipes = [];  
+          }
+          this.recipesChanged.next(this.recipes.slice())
+          return this.recipes.slice();
+        }
+      )
+    )
   }
 
-  public setRecipeList = (recipes: Recipe[]): void => {
-    this.recipes = recipes;
-    this.recipesChanged.next(this.recipes.slice());
-  }
-
-  public getSelectedRecipe = (id: number): Recipe => {
-    return this.recipes.find(recipe => recipe.id === id);
+  public getSelectedRecipe = (id: string): Observable<any> => {
+    return this.http.get(`${this.recipesURL}/${id}.json`).pipe(
+      map(
+        (res: Recipe) => {
+          res.id = id;
+          if (!res.ingredients) {
+            res.ingredients = [];
+          }
+          return res;
+        }
+      )
+    )
   }
 
   public addRecipe = (recipe: Recipe): void => {
-    recipe.id = this.generatedId;
-    this.recipes.push(recipe);
-    this.generatedId++;
-    this.recipesChanged.next(this.recipes.slice());
+    this.http.post(`${this.recipesURL}.json`, recipe)
+    .subscribe(
+      (res: {name: string}) => {
+        recipe.id = res.name
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+      },
+      error => {
+        console.log(error)
+      }
+    );
   }
 
   public deleteRecipe = (recipe: Recipe): void => {
-    this.recipes = this.recipes.filter(existingRecipe => existingRecipe.id !== recipe.id);
-    this.recipesChanged.next(this.recipes.slice());
+    this.http.delete(`${this.recipesURL}/${recipe.id}.json`)
+      .subscribe(
+        res => {
+          this.recipes = this.recipes.filter(existingRecipe => existingRecipe.id !== recipe.id);
+          this.recipesChanged.next(this.recipes.slice())
+        },
+        error => {
+          console.log(error);
+        }
+      );
   }
 
   public changeRecipe = (recipe: Recipe): void => {
-    (this.recipes as Recipe[]) = this.recipes.map((existingRecipe: Recipe) : Recipe => {
-      return existingRecipe.id === recipe.id ? recipe : existingRecipe;
-    });
-    this.recipesChanged.next(this.recipes.slice());
+    this.http.patch(`${this.recipesURL}/${recipe.id}.json`, recipe)
+    .subscribe(
+        (recipe: Recipe) => {
+          this.recipes = this.recipes.map(existingRecipe => {
+            return existingRecipe.id === recipe.id ? recipe : existingRecipe
+          });
+          this.recipeChanged.next(recipe);
+          this.recipesChanged.next(this.recipes.slice())
+        },
+        error => {
+          console.log(error);
+        }
+      )
   }
 }
