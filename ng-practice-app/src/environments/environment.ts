426e5ac147652e4380d6073158860cf094ab4ddd
forty9unbeaten <rob.sp3ars@gmail.com>
2021-04-12 11:49:00 -0400

    implement explivit typing

--- ng-practice-app/src/environments/environment.ts
+++ ng-practice-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const environment = {
+export const environment: { production: boolean; firebaseAPIKey: string } = {
   production: false,
-  firebaseAPIKey: 'replace this value',
+  firebaseAPIKey: null,
 };
