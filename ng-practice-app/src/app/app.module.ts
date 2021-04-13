296382704659d1d83ae3dc82584f660b624a5818
forty9unbeaten <rob.sp3ars@gmail.com>
2021-04-13 14:13:49 -0400

    implement ngRx store

--- ng-practice-app/src/app/app.module.ts
+++ ng-practice-app/src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 import { SharedResourceModule } from './shared/shared-resource.module';
+import { reducerMap } from './app.state';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, SharedResourceModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot(reducerMap),
+    SharedResourceModule,
+    AppRoutingModule,
+  ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
