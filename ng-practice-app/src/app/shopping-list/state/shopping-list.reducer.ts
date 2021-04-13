2fe3af1c1093aef44303a4205c0b374576ae54a9
forty9unbeaten <rob.sp3ars@gmail.com>
2021-04-13 14:16:17 -0400

    implement reducer for shopping list state

---
+++ ng-practice-app/src/app/shopping-list/state/shopping-list.reducer.ts
@@ -0,0 +1,84 @@
+import { Ingredient } from 'src/app/models/ingredient.model';
+import * as slActions from './shopping-list.actions';
+
+export interface ShoppingListState {
+  ingredients: Ingredient[];
+  selectedIngredient: Ingredient;
+  selectedIndex: number;
+}
+
+const initialState: ShoppingListState = {
+  ingredients: [],
+  selectedIngredient: null,
+  selectedIndex: -1,
+};
+
+export const shoppingListReducer = (
+  state = initialState,
+  action: slActions.ShoppingListActions
+) => {
+  switch (action.type) {
+    case slActions.ADD_INGREDIENT: {
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload],
+      };
+    }
+
+    case slActions.ADD_INGREDIENTS: {
+      const ingredients = [...state.ingredients];
+
+      action.payload.forEach((newIngredient) => {
+        // find duplicates and increase existing quantities
+        const existingIndex = ingredients.indexOf(newIngredient);
+        if (existingIndex === -1) {
+          ingredients.push(newIngredient);
+        } else {
+          ingredients[existingIndex].quantity += newIngredient.quantity;
+        }
+      });
+
+      return {
+        ...state,
+        ingredients: ingredients,
+      };
+    }
+
+    case slActions.EDIT_INGREDIENT: {
+      const updatedIngredient = {
+        ...state.ingredients[action.payload.index],
+        ...action.payload.ingredient,
+      };
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[action.payload.index] = updatedIngredient;
+
+      return {
+        ...state,
+        ingredients: updatedIngredients,
+      };
+    }
+
+    case slActions.DELETE_INGREDIENT: {
+      return {
+        ...state,
+        ingredients: state.ingredients.filter(
+          (existingIngredient, index) => index !== action.payload
+        ),
+      };
+    }
+
+    case slActions.SELECT_INGREDIENT: {
+      return {
+        ...state,
+        selectedIngredient: action.payload.ingredient,
+        selectedIndex: action.payload.index,
+      };
+    }
+
+    default: {
+      return {
+        ...state,
+      };
+    }
+  }
+};
