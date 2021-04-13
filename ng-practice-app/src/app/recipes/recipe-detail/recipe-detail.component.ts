bc150adb13a3fe32383b30f5e7176ccb8aa5916c
forty9unbeaten <rob.sp3ars@gmail.com>
2021-04-13 14:17:44 -0400

    adding ingredients to shopping list interacts with ngRx store

--- ng-practice-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ ng-practice-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,72 +1,67 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router} from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/models/ingredient.model';
 
 import { Recipe } from 'src/app/models/recipe.model';
 import { RecipesService } from 'src/app/services/recipes.service';
-import { ShoppingListService } from 'src/app/services/shopping-list.service';
+import * as slActions from '../../shopping-list/state/shopping-list.actions';
 
 @Component({
-	selector: 'app-recipe-detail',
-	templateUrl: './recipe-detail.component.html',
-	styleUrls: ['./recipe-detail.component.css']
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit, OnDestroy {
-	public recipe: Recipe;
-	public dataLoading = false;
-	private routeSub: Subscription;
-	private recipeSub: Subscription;
-	
-	constructor(
-		private recipesService: RecipesService,
-		private shoppingList: ShoppingListService,
-		private router: Router,
-		private currentRoute: ActivatedRoute) {}
+  public recipe: Recipe;
+  public dataLoading = false;
+  private routeSub: Subscription;
+  private recipeSub: Subscription;
 
-	ngOnInit(): void {
-		this.dataLoading = true;
-		this.routeSub = this.currentRoute.params.subscribe((params: Params) => {
-			this.recipesService.getSelectedRecipe(params.id)
-				.subscribe(
-					(recipe: Recipe) => {
-						if (recipe) {
-							this.recipe = recipe;
-							this.dataLoading = false;
-						} else {
-							this.router.navigate(['/not-found'])
-						}
-					},
-					error => {
-						console.log(error);
-					}
-				)
-		});
-		this.recipeSub = this.recipesService.recipeChanged.subscribe(
-			(recipe: Recipe) => {
-				this.recipe = recipe
-			}
-		)
-		
-	}
+  constructor(
+    private recipesService: RecipesService,
+    private router: Router,
+    private currentRoute: ActivatedRoute,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+  ) {}
 
-	ngOnDestroy(): void {
-		this.routeSub.unsubscribe();
-		this.recipeSub.unsubscribe();
-	}
+  ngOnInit(): void {
+    this.dataLoading = true;
+    this.routeSub = this.currentRoute.params.subscribe((params: Params) => {
+      this.recipesService.getSelectedRecipe(params.id).subscribe(
+        (recipe: Recipe) => {
+          if (recipe) {
+            this.recipe = recipe;
+            this.dataLoading = false;
+          } else {
+            this.router.navigate(['/not-found']);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    });
+    this.recipeSub = this.recipesService.recipeChanged.subscribe(
+      (recipe: Recipe) => {
+        this.recipe = recipe;
+      }
+    );
+  }
 
-	public onDeleteRecipe = (): void => {
-		this.recipesService.deleteRecipe(this.recipe);
-		this.router.navigate(['/recipes']);
-	}
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+    this.recipeSub.unsubscribe();
+  }
 
-	public onAddToShoppingList = (): void => {
-		this.shoppingList.addIngredients(
-			this.recipe.ingredients.map(ingredient => {
-				return new Ingredient(ingredient.name, ingredient.quantity, ingredient.measurementUnit);
-			})
-		);
-		this.router.navigate(['/shopping-list']);
-	}
+  public onDeleteRecipe = (): void => {
+    this.recipesService.deleteRecipe(this.recipe);
+    this.router.navigate(['/recipes']);
+  };
 
+  public onAddToShoppingList = (): void => {
+    this.store.dispatch(new slActions.AddIngredients(this.recipe.ingredients));
+    this.router.navigate(['/shopping-list']);
+  };
 }
