959f6b297931667bc984f074561c2bd3b58b8f91
forty9unbeaten <rob.sp3ars@gmail.com>
2021-04-14 10:49:46 -0400

    guard pulls user data from ngRx store

--- ng-practice-app/src/app/shared/guards/auth.guard.ts
+++ ng-practice-app/src/app/shared/guards/auth.guard.ts
@@ -8,15 +8,16 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
-import { User } from '@models/user.model';
-import { AuthService } from '@services/auth.service';
+import { AppState } from '@app/app.state';
+import * as fromAuth from '@auth/state';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private router: Router, private store: Store<AppState>) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -26,10 +27,10 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return this.authService.user.pipe(
+    return this.store.select('auth').pipe(
       take(1),
-      map((user: User) => {
-        if (user) {
+      map((authState: fromAuth.AuthState) => {
+        if (authState.user) {
           return true;
         }
         return this.router.createUrlTree(['/auth']);
