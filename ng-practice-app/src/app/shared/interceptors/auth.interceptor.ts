29b81cd5492aad3a5ea189c3e1428bddc0cde9d0
forty9unbeaten <rob.sp3ars@gmail.com>
2021-04-14 10:50:00 -0400

    interceptor pulls user data from ngRx store

--- ng-practice-app/src/app/shared/interceptors/auth.interceptor.ts
+++ ng-practice-app/src/app/shared/interceptors/auth.interceptor.ts
@@ -7,25 +7,31 @@ import {
   HttpParams,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { take, exhaustMap } from 'rxjs/operators';
+import { take, map, exhaustMap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 import { AuthService } from '@services/auth.service';
 import { User } from '@models/user.model';
+import { AppState } from '@app/app.state';
+import * as fromAuth from '@auth/state';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private store: Store<AppState>
+  ) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return this.authService.user.pipe(
+    return this.store.select('auth').pipe(
       take(1),
-      exhaustMap((user: User) => {
-        if (user) {
+      exhaustMap((authState: fromAuth.AuthState) => {
+        if (authState.user) {
           const reqClone = request.clone({
-            params: new HttpParams().set('auth', user.token),
+            params: new HttpParams().set('auth', authState.user.token),
           });
           return next.handle(reqClone);
         }
