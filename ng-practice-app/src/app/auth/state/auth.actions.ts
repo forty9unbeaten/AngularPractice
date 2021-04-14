7cc04891eddccc65625c4bdcd9a2d20ebda0bade
forty9unbeaten <rob.sp3ars@gmail.com>
2021-04-14 10:46:53 -0400

    login payload now comprised of data to create new user object

--- ng-practice-app/src/app/auth/state/auth.actions.ts
+++ ng-practice-app/src/app/auth/state/auth.actions.ts
@@ -1,12 +1,18 @@
 import { Action } from '@ngrx/store';
-import { User } from 'src/app/models/user.model';
 
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 
 export class Login implements Action {
   readonly type = LOGIN;
-  constructor(public payload: User) {}
+  constructor(
+    public payload: {
+      email: string;
+      userId: string;
+      token: string;
+      expirationDate: Date;
+    }
+  ) {}
 }
 
 export class Logout implements Action {
