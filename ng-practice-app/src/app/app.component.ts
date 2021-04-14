3a5b85beca4b31cf9e7cbd9628a9428ce50939f6
forty9unbeaten <rob.sp3ars@gmail.com>
2021-04-14 10:22:34 -0400

    remove unused imports

--- ng-practice-app/src/app/app.component.ts
+++ ng-practice-app/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ShoppingListService } from './services/shopping-list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent implements OnInit {
   public title = 'ng-practice-app';
 
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
